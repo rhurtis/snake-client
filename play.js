61a1794722e10423ebb6d458336df81cfcbc907c
Ryan H <rhurtis@hotmail.com>
2020-07-29 19:26:16 +0000

    Added basic user input and ensured that the game could end with ctrl+c.

--- play.js
+++ play.js
@@ -8,10 +8,37 @@ console.log('Connecting ...');
 
 connect();
 
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', (key) => {
+    process.stdout.write('.');
+    
+    const handleUserInput = function() {
+      // \u0003 maps to ctrl+c input
+      if (key === '\u0003') {
+        process.exit();
+      }
+    };
+    handleUserInput();
+  });
+  
+  
+
+
+  return stdin;
+};
 
 
 
 
 
-
+setupInput();
 
