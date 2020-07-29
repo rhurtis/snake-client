3cbfa2a925efb5eebeb91c5979968fa8473d1ab9
Ryan H <rhurtis@hotmail.com>
2020-07-29 19:33:29 +0000

    Refactored the code by importing/exporting the input functions.

--- play.js
+++ play.js
@@ -3,41 +3,11 @@
 //PORT: 50541
 
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 
 console.log('Connecting ...');
-
 connect();
-
-/**
- * Setup User Interface
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  stdin.on('data', (key) => {
-    process.stdout.write('.');
-    
-    const handleUserInput = function() {
-      // \u0003 maps to ctrl+c input
-      if (key === '\u0003') {
-        process.exit();
-      }
-    };
-    handleUserInput();
-  });
-  
-  
-
-
-  return stdin;
-};
-
-
-
 
 
 setupInput();
